4233cc72e7035eea02842ea13d2c3a65adafceea
antoinecharlet <tou-toine@hotmail.fr>
2015-01-20 22:05:26 +0100

    page accueil(html,css)

--- src/fifa/C1.java
+++ src/fifa/C1.java
@@ -59,15 +59,8 @@ boolean pouleJoue=false;
     ArrayList<Equipe> initEquipe() {
          {//fonction qui recuper les donnée de la BD
         ArrayList<Equipe> tmp = new ArrayList();
-        tmp.add(new Equipe("lapin", "FrancE"));
-        tmp.add(new Equipe("lapin1", ""));
-        tmp.add(new Equipe("lapin2", "cou4cou"));
-        tmp.add(new Equipe("lapin3", "couco4u"));
-        tmp.add(new Equipe("lapin4", "coucou"));
-        tmp.add(new Equipe("lapin5", "coucou"));
-        tmp.add(new Equipe("lapin6", "coucou"));
-        tmp.add(new Equipe("lapin7", "coucou"));
-        for (int i = 0; i < 8; i++) {
+        
+        for (int i = 0; i < 16; i++) {
             tmp.add(new Equipe("lapin" + (8 + i), "coucou"));
         }
         equipe = tmp;
